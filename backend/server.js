93590630894cd4397be47cc477abda87608704e9
ciapjl <CPPJEA001@myuct.ac.za>
2022-03-22 23:57:54 +0200

    Initial commit with backend file structure

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const userRouter = require('./routes/user.router.js')
+
+
+const app = express()
+
+mongoose.connect(process.env.MongoDB_URI, {useNewUrlParser: true})
+
+const db = mongoose.connection
+
+db.once('open', ()=> console.log('Database is connected and running'))
+
+
+
+
+app.use(express.json())
+
+
+
+
+app.listen(5000, ()=> console.log('Server is listening on 5000'))
